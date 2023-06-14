604a594f222ad8e8378025ea9f8e71382079214d
ikramKOU <104200410+ikramKOU@users.noreply.github.com>
2023-06-14 19:10:37 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import Landig_page from  './Landig_page';
+
+function App(){
+  return <div><Landig_page /></div>;
+}
+export default App;
---
+++ src/Landig_page.tsx
@@ -0,0 +1,256 @@
+
+import React from 'react'
+import './Landig_page.css';
+function Landig_page(){
+
+return (	
+<>
+
+
+<head>
+        <title>ISSI School</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+     
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <noscript>
+          <link rel="stylesheet" href="assets/css/noscript.css" />
+        </noscript>
+        <script src="public/js/jquery.min.js"></script>
+<script src="public/js/jquery.scrolly.min.js"></script>
+<script src="public/js/browser.min.js"></script>
+<script src="public/js/breakpoints.min.js"></script>
+<script src="public/js/util.js"></script>
+<script src="public/js/main.js"></script>
+</head>
+<body className="is-preload">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <a href="C:/Users/ikram/OneDrive/Bureau/pfa/Dash/src/pages/samples/login.html" className="btn btn-secondary">
+                Connexion
+              </a>
+            </div>
+            <div className="col-md-6">
+              <a href="C:/Users/ikram/OneDrive/Bureau/pfa/Dash/src/pages/samples/register.html" className="btn btn-secondary">
+                Pré-Inscription
+              </a>
+            </div>
+            <div className="col-md-6">
+              <a href="C:/Users/ikram/OneDrive/Bureau/pfa/Dash/src/pages/samples/registerProf.html" className="btn btn-secondary">
+                Inscription Professeur
+              </a>
+            </div>
+            <div className="col-md-6">
+              <a href="C:/Users/ikram/OneDrive/Bureau/pfa/Dash/src/pages/samples/contact.html" className="btn btn-secondary">
+                Contact
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <section id="header">
+          <div className="inner">
+            <span>
+              <img src="../assets/images/lofin.png" alt="" />
+            </span>
+            <h1>
+              PLONGEZ DANS L’EXCELLENCE EDUCATIVE
+              <br />
+              Bienvenue <a href="#"></a>.
+            </h1>
+            <p>
+            découvrir les nouvelles technologies de l'information constitue <br />
+              un levier de développement de l’innovation
+            </p>
+            <ul className="actions special">
+              <li>
+                <a href="#" className="button scrolly">
+                  décovrir notre groupe scolaire
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section id="one" className="main style1">
+          <div className="container">
+            <div className="row gtr-150">
+              <div className="col-6 col-12-medium">
+                <header className="major">
+                  <h2>Institut Suprérieur<br />Des science de l'ingénieur</h2>
+                </header>
+                <p>
+                  ISSI est le premier institut du digital qui s'engage sur l'emploi, le fruit d'un partenariat des acteurs académiques et professionnels, ISSI développe
+                  des cursus de formations basés sur les besoins du marché et orienté vers les certifications internationales..
+                </p>
+              </div>
+              <div className="col-6 col-12-medium imp-medium">
+                <span className="image fit">
+                  <img src="images/kl.png" alt="" />
+                </span>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="two" className="main style2">
+<div className="container">
+<div className="row gtr-150">
+<div className="col-6 col-12-medium">
+<ul className="major-icons">
+<li>
+<span className="icon solid style1 major fa-code"></span>
+</li>
+<li>
+<span className="icon solid style2 major fa-bolt"></span>
+</li>
+<li>
+<span className="icon solid style3 major fa-camera-retro"></span>
+</li>
+<li>
+<span className="icon solid style4 major fa-cog"></span>
+</li>
+<li>
+<span className="icon solid style5 major fa-desktop"></span>
+</li>
+<li>
+<span className="icon solid style6 major fa-calendar"></span>
+</li>
+</ul>
+</div>
+<div className="col-6 col-12-medium">
+<header className="major">
+<h2>
+Formation<br />
+Systeme de l'information et la transformation degitale
+</h2>
+</header>
+<p></p>
+
+</div>
+</div>
+</div>
+</section>
+
+<section id="three" className="main style1 special">
+      <div className="container">
+        <header className="major">
+          <h2>Cours Standard</h2>
+        </header>
+        <p>PLONGEZ DANS L’EXCELLENCE EDUCATIVE QVEC NOS COURS EN LIGNE.</p>
+        <div className="row gtr-150">
+          <div className="col-4 col-12-medium">
+            <span className="image fit">
+              <img src="images/C1.jfif" alt="" />
+            </span>
+            <h3>c1</h3>
+            <p>description cours.</p>
+            <ul className="actions special">
+              <li>
+                <a href="#" className="button">
+                  plus
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-4 col-12-medium">
+            <span
+            className="image fit">
+            <img src="images/C2.jfif" alt="" />
+            </span>
+            <h3>c2</h3>
+            <p>description cours.</p>
+            <ul className="actions special">
+            <li>
+            <a href="#" className="button">
+            plus
+            </a>
+            </li>
+            </ul>
+            </div>
+            <div className="col-4 col-12-medium">
+            <span className="image fit">
+            <img src="images/C3.jfif" alt="" />
+            </span>
+            <h3>c3</h3>
+            <p>description cours.</p>
+            <ul className="actions special">
+            <li>
+            <a href="#" className="button">
+            Plus
+            </a>
+            </li>
+            </ul>
+            </div>
+            </div>
+            </div>
+            </section>    <section id="four" className="main style2 special">
+      <div className="container">
+        <header className="major">
+          <h2>Contactez-nous</h2>
+        </header>
+        <p>
+          Ne manquez rien, inscrivez-vous maintenant et restez informé sur tout ce qui concerne notre école.
+        </p>
+        <ul className="actions special">
+          <li>
+            <a href="#" className="button wide primary">
+              Sign Up
+            </a>
+          </li>
+          <li>
+            <a href="#" className="button wide">
+              Voir plus
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section id="footer">
+      <ul className="icons">
+        <li>
+          <a href="#" className="icon brands alt fa-twitter">
+            <span className="label">Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon brands alt fa-facebook-f">
+            <span className="label">Facebook</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon brands alt fa-instagram">
+            <span className="label">Instagram</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon brands alt fa-github">
+            <span className="label">GitHub</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon solid alt fa-envelope">
+            <span className="label">Email</span>
+          </a>
+        </li>
+      </ul>
+      <ul className="copyright">
+        <li>&copy; IKRAMISTA_kaw</li>
+        <li>PFA_2023</li>
+      </ul>
+    </section>
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.scrolly.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+  </body>
+
+
+
+</>
+)
+}
+export default  Landig_page;
